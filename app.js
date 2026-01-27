cc536b7f546e9aeec27c4574e304f5951f57bd9d
got <pongsathorn0033@gmail.com>
2026-01-27 20:38:29 +0700

    middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 
+const errorMiddleware = require ("./middleware/error.middleware.js")
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(errorMiddleware);
 
 const loanRoute = require("./modules/loan/loan.route");
 app.use("/api/loans", loanRoute);
