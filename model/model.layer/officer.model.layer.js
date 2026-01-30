cd59c5b36733ef7542274ff6bff42be9f0ad79d7
got <pongsathorn0033@gmail.com>
2026-01-30 15:54:46 +0700

    addfficer fix

--- model/model.layer/officer.model.layer.js
+++ model/model.layer/officer.model.layer.js
@@ -1,20 +1,46 @@
-// modules/officer/officer.model.js
-const prisma = require('../../config/prisma')
+const pool = require('../../db')
+const bcrypt = require('bcrypt')
 
-const createOfficer = (data) => {
-  return prisma.officer.create({
-    data: {
-      officerCode: data.officerCode,
-      name: data.name,
-      email: data.email,
-      institute: data.institute,
-      jobPosition: data.jobPosition,
-      phone: data.phone,
-      role: data.role
-    }
-  })
+const createOfficer = async (data) => {
+  try {
+    const {
+      officerCode,
+      name,
+      username,
+      password,
+      email,
+      institute,
+      jobPosition,
+      phone,
+      role
+    } = data
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const result = await pool.query(
+      `
+      INSERT INTO officers 
+      (officer_code, name, username, password, email, institute, job_position, phone, role)
+      VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
+      RETURNING id, officer_code, name, username, email, role
+      `,
+      [
+        officerCode,
+        name,
+        username,
+        hashedPassword,
+        email,
+        institute,
+        jobPosition,
+        phone,
+        role
+      ]
+    )
+
+    return result.rows[0]
+  } catch (err) {
+    throw err
+  }
 }
 
-module.exports = {
-  createOfficer
-}
+module.exports = { createOfficer }
